c49fc6400310df66f230f70c6f11590ee2e9fe9f
Fritz Obermeyer <fritz.obermeyer@gmail.com>
2016-02-28 19:21:48 -0800

    Sketch engine and io

--- src/engine.h
+++ src/engine.h
@@ -1,3 +1,10 @@
 #pragma once
 
-void reduce();
+/* Ob is a 1-based pointer type; 0 denotes null. */
+typedef uint32_t Ob;
+
+Ob hstar_simplify_app(Ob lhs, Ob rhs);
+Ob hstar_reduce_app(Ob lhs, Ob rhs, int* budget);
+
+Ob hstar_simplify(Ob ob);
+Ob hstar_reduce(Ob ob, int* budget);
---
+++ src/io.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "engine.h"
+
+Ob hstar_parse(const char* input);
+
+const char* hstar_print(Ob ob);

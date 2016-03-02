2e137c970faa9fbbec56952c1430cc49c741213b
Fritz Obermeyer <fritz.obermeyer@gmail.com>
2016-03-01 20:03:55 -0800

    Fix compile errors in engine.*

--- src/engine.c
+++ src/engine.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <strings.h>
+#include <string.h>
 
 #ifdef __GNUG__
 #define likely(x) __builtin_expect(!!(x), true)
@@ -35,6 +36,7 @@
   { if (unlikely(!(cond)) UN_ERROR( ##__VA_ARGS__ ) }
 #endif  // NDEBUG
 
+#define UN_ASSERT_TRUE(cond) UN_ASSERT(cond, "failed assertion: " #cond)
 #define UN_ASSERT_OP(x, op, y)                                               \
   UN_ASSERT((x)op(y), "expected " #x " " #op " " #y "; actual %ull vs %ull", \
             (x), (y))
@@ -74,25 +76,25 @@ typedef struct {
 } ObPair;
 _Static_assert(sizeof(ObPair) == 8, "OpPair has wrong size");
 
-union Word {
+typedef union {
   Ob ob;
   ObPair ob_pair;
   uint8_t uint8s[8];
   uint16_t uint16s[4];
   uint32_t uint32s[2];
   uint64_t uint64s[1];
-};
+} Word;
 _Static_assert(sizeof(Word) == 8, "Word has wrong size");
 
 inline uint64_t Word_hash(const Word* word) { return hash_64(word->num); }
 
-union Hash_Node {
+typedef union {
   Word key;
   uint8_t uint8s[16];
   uint16_t uint16s[8];
   uint32_t uint32s[4];
   uint64_t uint64s[2];
-};
+} Hash_Node;
 _Static_assert(sizeof(Hash_Node) == 16, "Hash_Node has wrong size");
 
 inline uint64_t Hash_Node_hash(const Hash_Node* node) {
@@ -107,15 +109,15 @@ typedef struct {
 } Hash;
 
 static void Hash_validate(const Hash* hash) {
-  UN_ASSERT(is_power_of_2(hash->size));
+  UN_ASSERT_TRUE(is_power_of_2(hash->size));
   UN_ASSERT_LT(hash->count, hash->size);
   UN_ASSERT_EQ(hash->mask, hash->size - 1UL);
-  UN_ASSERT(hash->nodes)
+  UN_ASSERT_TRUE(hash->nodes)
 }
 
 static Info Hash_init(Hash* hash, size_t size) {
   if (unlikely(!is_power_of_2(size))) {
-    fprintf(stderr, "expected size a power of 2, actual %ull", size);
+    fprintf(stderr, "expected size a power of 2, actual %lu", size);
     return info;
   }
   const size_t bytes = sizeof(Hash_Node) * size;
@@ -131,15 +133,15 @@ static Info Hash_init(Hash* hash, size_t size) {
   return Info_success;
 }
 
-inline void Hash_insert_nogrow(Hash* hash, const Hash_Node* node);
+inline Hash_Node* Hash_insert_nogrow(Hash* hash, const Hash_Node* node_to_insert);
 
 static Info Hash_grow(Hash* hash) {
   Hash grown;
-  Info info = Hash_init(grown, hash->size * 2UL);
+  Info info = Hash_init(&grown, hash->size * 2UL);
   if (unlikely(info)) return info;
   for (Hash_Node* node = hash->nodes, *end = node + hash->size; ++node) {
     if (node->key.uint64s[0]) {
-      Hash_insert_nogrow(grown, node);
+      Hash_insert_nogrow(&grown, node);
     }
   }
   free(hash->nodes);
@@ -166,7 +168,7 @@ static Hash_Node* Hash_find(const Hash* hash, Word key) {
 }
 
 // Returns pointer on success, else NULL.
-static Hash_Node* node Hash_insert(Hash* hash, const Hash_Node* node) {
+static Hash_Node* Hash_insert(Hash* hash, const Hash_Node* node) {
   if (unlikely(hash->count * 2UL == hash->size)) {
     Info info = Hash_grow(hash);
     if (unlikely(info)) {
@@ -177,13 +179,13 @@ static Hash_Node* node Hash_insert(Hash* hash, const Hash_Node* node) {
 }
 
 // Returns pointer to inserted node. Assumes node is not already inserted.
-static Hash_Node* Hash_insert(Hash* hash, const Hash_Node* node_to_insert) {
+inline Hash_Node* Hash_insert_nogrow(Hash* hash, const Hash_Node* node_to_insert) {
   Hash_Node found;
   uint64_t pos = Hash_bucket(hash, key);
   Hash_Node* node = hash->val[pos];
   UN_ASSERT_LT(hash->count, hash->size);  // Required for termination.
   while (node->uint64s[0]) {
-    UN_ASSERT_NE(node->uint64[0], node_to_insert->uint64s[0]);
+    UN_ASSERT_NE(node->uint64s[0], node_to_insert->uint64s[0]);
     pos = (pos + 1UL) & hash->mask;
     node = hash->val[pos];
   }
@@ -205,7 +207,7 @@ static Hash_Node* Hash_insert(Hash* hash, const Hash_Node* node_to_insert) {
 // - reduced : Ob -partial-> Ob
 // - pending : ReprioritizableQueue Ob
 
-struct Structure {
+typedef struct {
   Hash hash;  // A shared associative array.
 
   InverseHash app_Lrv;
@@ -213,10 +215,10 @@ struct Structure {
   InverseHash app_Vlr;
 
   Hash abs_LRv;
-};
+} Structure;
 
-Info Structure_init(Structure* structure) {
-  Info info = Hash_init(&structure->hash);
+Info Structure_init(Structure* structure, size_t size) {
+  Info info = Hash_init(&structure->hash, size);
   if (unlikely(info)) return info;
   TODO("init other structure");
 }
@@ -237,8 +239,8 @@ static Ob compute_app(Ob lhs, Ob rhs, int budget) { TODO("implement"); }
 // Interface
 
 // Returns 0 on success.
-int un_init() {
-  Info info = Structure_init(&g_structure);
+int un_init(size_t size) {
+  Info info = Structure_init(&g_structure, size);
   if (unlikely(info)) return info;
   return Info_success;
 }
--- src/engine.h
+++ src/engine.h
@@ -1,10 +1,12 @@
 #pragma once
 
+#include <stdint.h>
+
 /* Ob is a 1-based pointer type; 0 denotes null. */
 typedef uint32_t Ob;
 
 // Returns 0 on success.
-int un_init();
+int un_init(size_t size);
 
 Ob un_simplify_app(Ob lhs, Ob rhs);
 Ob un_reduce_app(Ob lhs, Ob rhs, int* budget);
